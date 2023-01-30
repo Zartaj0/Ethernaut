44635403ec89a2c6fd8907a647c51c776f236f7c
zartaj <zartaj.afsar@gmail.com>
2023-01-30 15:30:07 +0400

    script to read storage

---
+++ scripts/AttackVault.js
@@ -0,0 +1,32 @@
+
+
+let address;
+async function main() {
+
+
+  address = "0x15DE5d9EAFad20EbC3A572954459d7085A000e4d";
+
+ 
+
+
+  
+  const a = await ethers.provider.getStorageAt(address, 1);
+  console.log(a);
+
+  
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
+async function getShortStr() {
+
+
+
+
+}
+
+getShortStr(0);
+
